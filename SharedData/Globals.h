bf59d326b31409f04041b88ed4ee7e39b439beca
Indeed <gravitybartek@gmail.com>
2025-01-26 15:18:18 +0100

    Fix SIGKILL trying to be captured

--- SharedData/Globals.h
+++ SharedData/Globals.h
@@ -8,12 +8,12 @@ constexpr auto SHM_NAME = "/my_shm";
 constexpr auto SEM_PRODUCER_PREFIX = "/my_producer_sem_";
 constexpr auto SEM_RECEIVER_MUTEX = "/my_receiver_mutex";
 constexpr auto SEM_GLOBAL_MUTEX = "/my_global_mutex";
-constexpr int MAX_PRODUCERS = 10;
+constexpr int MAX_PRODUCERS = 3;
 constexpr size_t SHM_SIZE = 1024;
 constexpr int STOP_RECEIVER_SIGNAL = SIGUSR1;
 constexpr int STOP_PRODUCER_SIGNAL = SIGUSR2;
 constexpr int STOP_ALL_SIGNAL = SIGTERM;
-constexpr int STOP_ALL_WITH_SAVE_SIGNAL = SIGKILL;
+constexpr int STOP_ALL_WITH_SAVE_SIGNAL = SIGINT;
 extern Logger logger;
 
 #endif // GLOBALS_H
